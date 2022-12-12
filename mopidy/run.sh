2dd17a81b7b33287ddf490079f70c4bafb6c0f45
Valux <marco.valvano@live.de>
2022-12-12 23:08:59 +0100

    le voix

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -15,7 +15,8 @@ config="/share/mopidy/mopidy.conf"
 
 bashio::log.info 'Start Mopidy....'
 if  [ "$local_scan" == "true" ]; then
+    sudo -u icecast2 bash -c "icecast2 -c /etc/icecast2/icecast2.xml"
     mopidy --config "$config" "$options" local scan
 fi
-sudo -u icecast2 bash -c icecast2 -c /etc/icecast2/icecast2.xml
+sudo -u icecast2 bash -c "icecast2 -c /etc/icecast2/icecast.xml"
 mopidy --config "$config" "$options"
