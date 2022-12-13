41dba2770065f2641868752f6376c1e7028fcdbb
Valux <marco.valvano@live.de>
2022-12-13 21:31:48 +0100

    generic changes

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -12,9 +12,9 @@ bashio::log.info 'Setup config'
 local_scan=$(cat /data/options.json | jq -r '.local_scan // empty')
 options=$(cat /data/options.json | jq -r 'if .options then [.options[] | "-o "+.name+"="+.value ] | join(" ") else "" end')
 config="/share/mopidy/mopidy.conf"
-
-
+bashio::log.info 'Start Icecast....'
 sudo -E -u icecast2 bash -c "icecast2 -b -c /etc/icecast2/icecast.xml"
+
 bashio::log.info 'Start Mopidy....'
 if  [ "$local_scan" == "true" ]; then
     mopidy --config "$config" "$options" local scan
